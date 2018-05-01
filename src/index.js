6030b6ecd5a3e545987877a31f325d823471a8d9
Yufan Wang <yufanwangcn@hotmail.com>
2018-05-01 16:51:26 -0700

    added router to redux state

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {createStore} from 'redux';
-import 'babel-polyfill'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import 'babel-polyfill';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App'
 import reducers from './reducers';
 
-// just bring in sdome intial data to seed the app
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter as Router, routerMiddleware } from 'react-router-redux'
 import initialState from './data/initialState';
 
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+
 const store = createStore(
-    reducers,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducers,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 const node1 = document.getElementById('app');
 
 if (node1) {
-    ReactDOM.render(
-        <Router>
-            <App store={store}/>
-        </Router>,
-        node1);
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router history={history}>
+        <App store={store} />
+      </Router>
+    </Provider>,
+    node1);
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import peopleReducer from "./peopleReducer";
 import placesReducer from "./placesReducer";
+import {routerReducer} from 'react-router-redux';
 
 const rootReducer = combineReducers({
     people: peopleReducer,
     places: placesReducer,
+    router: routerReducer
 });
 
 export default rootReducer;
